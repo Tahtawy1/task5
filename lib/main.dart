d40d8544e6edcb7f66907b5d36845f14164c8dcf
Tahtawy1 <sabrytaha854@gmail.com>
2026-03-15 06:01:52 +0200

    finish task

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:nti_task5/screens/on_boarding_screen.dart';
+
+void main() {
+  runApp(
+    DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) => MyApp(), // Wrap your app
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(fontFamily: 'Tajawal'),
+      debugShowCheckedModeBanner: false,
+      home: const OnBoardingScreen(),
+    );
+  }
+}
